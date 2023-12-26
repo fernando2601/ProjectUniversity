9f686ac0f7081502990f0ec9521966d5274ca568
fernando <nerifernando2@gmail.com>
2023-12-26 00:53:45 -0300

    Health Check instalado e correção no metodo do repositorio da Disciplina

--- ProjectUniversity/Startup.cs
+++ ProjectUniversity/Startup.cs
@@ -1,10 +1,16 @@
+using Ioc.Extension;
 using IOC;
 using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.OpenApi.Models;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System.Linq;
 
 namespace ProjectUniversity
 {
@@ -27,9 +33,33 @@ namespace ProjectUniversity
 
             services.AdicionarServicos(connectionString);
 
-            // Add CORS, Swagger, Authentication, etc. if needed
+            services.ConfigurarAutenticacaoJwt();
+
+            services.AddCors(options =>
+            {
+                options.AddPolicy("CorsPolicy",
+                    builder => builder
+                        .AllowAnyOrigin()
+                        .AllowAnyMethod()
+                        .AllowAnyHeader());
+            });
+
 
             services.AddControllers();
+
+            services.AddHealthChecks()
+                .AddSqlServer(connectionString)
+                .AddCheck<SelfHealthCheck>("Self");
+
+            // Adicionar Health Checks UI com armazenamento em memória
+            services.AddHealthChecksUI()
+                .AddInMemoryStorage();
+
+            //services.AddHealthChecksUI(options =>
+            //{
+            //    options.AddHealthCheckEndpoint("Endpoint1", "/healthz");
+
+            //});
         }
 
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
@@ -53,6 +83,36 @@ namespace ProjectUniversity
             app.UseRouting();
 
             app.UseAuthorization();
+
+            // Use Health Checks middleware
+            app.UseHealthChecks("/status", new HealthCheckOptions
+            {
+                ResponseWriter = (httpContext, result) =>
+                {
+                    httpContext.Response.ContentType = "application/json";
+
+                    var json = new JObject(
+                        new JProperty("status", result.Status.ToString()),
+                        new JProperty("results", new JObject(result.Entries.Select(pair =>
+                            new JProperty(pair.Key, new JObject(
+                                new JProperty("status", pair.Value.Status.ToString()),
+                                new JProperty("description", pair.Value.Description),
+                                new JProperty("data", new JObject(pair.Value.Data.Select(
+                                    p => new JProperty(p.Key, p.Value))))))))));
+                    return httpContext.Response.WriteAsync(json.ToString(Formatting.Indented));
+                }
+            });
+
+            app.UseHealthChecksUI(options =>
+            {
+                options.UIPath = "/status-dashboard";
+            });
+
+            app.UseHealthChecks("/status-api", new HealthCheckOptions
+            {
+                Predicate = _ => true,
+                ResponseWriter = HealthChecks.UI.Client.UIResponseWriter.WriteHealthCheckUIResponse,
+            });
 
             app.UseEndpoints(endpoints =>
             {
